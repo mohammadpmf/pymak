54d7109a92dd63d8bc5a94ea78cae4740de096d2
poulstar <poulstar@gmail.ocm>
2022-09-10 18:58:45 +0430

    19 shahrivar

--- weblog/blog/models.py
+++ weblog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+# from django.contrib.auth.
 
-# Create your models here.
+class Post(models.Model):
+    CHOICES = (
+        ('pub', 'published'),
+        ('drf', 'draft'),
+    )
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=3, choices=CHOICES)
+    # author = models.ForeignKey()
+    
+    def __str__(self):
+        return f"{self.title}"
